582cd971852915757ffc51156e7df45cfc979059
jasma-balasangameshwara <jasma.balasangameshwara@gmail.com>
2021-12-25 10:10:15 +0530

    EDA Updated

---
+++ starter/eda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+census_df = pd.read_csv("data/raw/census.csv", skipinitialspace=True)
+census_df = census_df.dropna()
+census_df.replace({'?': None})
+census_df.to_csv("data/processed/cleaned_census.csv", index=False)
+
