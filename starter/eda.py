714b57e17bcaa6df092c1587681cec9a9c996e9a
jasma-balasangameshwara <jasma.balasangameshwara@gmail.com>
2021-12-29 15:36:15 +0530

    train_model.py updated

--- starter/eda.py
+++ starter/eda.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 
-def clean():
-    census_df = pd.read_csv("data/raw/census.csv", skipinitialspace=True)
+def clean(input_path, output_path):
+    census_df = pd.read_csv(input_path, skipinitialspace=True)
     census_df = census_df.dropna()
     census_df.replace({'?': None})
-    census_df.to_csv("data/processed/cleaned_census.csv", index=False)
+    census_df.to_csv(output_path, index=False)
     return census_df
+
+
+if __name__ == '__main__':
+    dataframe = clean("data/raw/census.csv", "data/processed/cleaned_census.csv")
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,29 +1,48 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
-from
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from sklearn.ensemble import GradientBoostingClassifier
+from joblib import dump
+from starter.eda import clean
+import scipy
+import numpy as np
+
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+def train_test():
+    data = clean(
+        "D:/CAREER TRANSITION/Udacity/MLOps - Nanodegree/Capstone Projects/FastAPI/nd0821-c3-starter-code-master/starter/data/raw/census.csv",
+        "D:/CAREER TRANSITION/Udacity/MLOps - Nanodegree/Capstone Projects/FastAPI/nd0821-c3-starter-code-master/starter/data/processed/cleaned_census.csv")
+    train_set, test = train_test_split(data, test_size=0.20)
+    return data, train_set, test
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
-
-# Proces the test data with the process_data function.
-
-# Train and save a model.
+def train_predict():
+    data, train, test = train_test()
+    categorical_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country", ]
+    y_train = train["salary"]
+    train = train.drop(["salary"], axis=1)
+    x_categorical = train[categorical_features].values
+    x_continuous = train.drop(*[categorical_features], axis=1)
+    encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
+    lb = LabelBinarizer()
+    x_categorical = encoder.fit_transform(x_categorical)
+    y_train = lb.fit_transform(y_train.values).ravel()
+    x_train = np.concatenate([x_continuous, x_categorical], axis=1)
+    model = GradientBoostingClassifier(n_estimators=100)
+    model.fit(x_train, y_train)
+    y_predicted = model.predict(test)
+    dump(model,
+         "D:/CAREER TRANSITION/Udacity/MLOps - Nanodegree/Capstone Projects/FastAPI/nd0821-c3-starter-code-master/starter/model/model.joblib")
+    return model, y_predicted
