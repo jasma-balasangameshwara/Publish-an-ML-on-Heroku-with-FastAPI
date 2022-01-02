6702e1d1a8c5c5b061dd48c5b7ecb196a05d97bb
jasma-balasangameshwara <jasma.balasangameshwara@gmail.com>
2022-01-02 12:25:26 +0530

    DVC AWS Connecting

--- starter/main.py
+++ starter/main.py
@@ -30,7 +30,8 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system('rm -rf .dvc/cache')
     os.system('rm -rf .dvc/tmp/lock')
     os.system('dvc config core.hardlink_lock true')
-    if os.system("dvc pull -q") != 0:
+    os.system("dvc remote add -d s3-bucket s3://ml-heroku-fastapi-bucket")
+    if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -rf .dvc .apt/usr/lib/dvc")
 
